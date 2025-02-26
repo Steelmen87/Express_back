ce700927e1fc0f329d70a36f03590fdf3da26d3e
Steel <Steelmen87@bk.ru>
2025-02-26 16:24:55 +0300

    add Middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express, {NextFunction, Request, Response} from "express";
 import {productsRoute} from "./routes/products-router";
 import {addressesRoute} from "./routes/adderess-router";
 
@@ -6,10 +6,22 @@ const app = express()
 const PORT = 5000
 
 
+//middleware
+let counterReq = 0
+const requestCounterMiddleware = (req: Request, res: Response,next:NextFunction)=>{
+    counterReq++
+    next()
+}
+
+
+//
+
+app.use(requestCounterMiddleware);
 app.use(express.json());
 app.use('/products',productsRoute)
 app.use('/addresses',addressesRoute)
 app.get('/', (req: Request, res: Response) => {
+    // res.send('This is back on Express'+' '+counterReq)
     res.send('This is back on Express')
 })
 
